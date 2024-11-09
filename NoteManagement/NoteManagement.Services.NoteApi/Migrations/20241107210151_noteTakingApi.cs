2cfdf2e0893ab07aa163ae183893892e33adcc61
unknown <swethaasp23@gmail.com>
2024-11-09 15:56:47 +0530

    added noteapi and untracked garbage

---
+++ NoteManagement/NoteManagement.Services.NoteApi/Migrations/20241107210151_noteTakingApi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteManagement.Services.NoteApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class noteTakingApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Resources = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
---
+++ NoteManagement/NoteManagement.Services.NoteApi/Repository/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using NoteManagement.Services.NoteApi.Models;
+
+namespace NoteManagement.Services.NoteApi.Repository
+{
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetNotesByUser(int userId);
+        Task<Note> GetNoteByCreatedDate(DateTime dateCreated);
+        Task<Note> GetNoteByModifiedDate(DateTime dateModified);
+        Task<Note> CreateNote(Note note);
+        Task<Note> UpdateNote(Note note);
+        Task<bool> DeleteNote(int id);
+    }
+}
